eb1948eaaaad812fb930c1aab3d15b8e31095696
Alexandr Kochurov <b2b4ly@gmail.com>
2023-04-25 13:26:11 +0400

    feat: add font

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "normalize.css";
-import "./assets/base.scss";
+import "./assets/styles/base.scss";
 
 const app = createApp(App);
 app.mount("#app");
